297206a683c1976345dd7a3d784b4ecaa06e7344
Jun <metalbaram@naver.com>
2025-04-27 19:30:49 +0900

    Remove ProjectPage component: Deleted the page.tsx file as part of project restructuring.

--- src/app/projects/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProjectDetail from '../../components/ProjectDetail';
-
-export default function ProjectPage({ params }: { params: { slug: string } }) {
-  const { slug } = params;
-  return <ProjectDetail slug={slug} />;
-} 
